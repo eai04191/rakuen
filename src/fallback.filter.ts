693fd9499bf98551fd745761a058a6850af57f12
Irie Aoi <eai@mizle.net>
2022-03-08 05:01:44 +0900

    fallbackにResultとSequenceをつけてごまかす

--- src/fallback.filter.ts
+++ src/fallback.filter.ts
@@ -8,8 +8,14 @@ import {
 @Catch()
 export class FallbackFilter implements ExceptionFilter {
     catch(exception: HttpException, host: ArgumentsHost) {
-        host.switchToHttp().getResponse().status(200).send({
-            ErrorCode: 0,
-        });
+        const Sequence = host.switchToHttp().getRequest().body.Sequence;
+        host.switchToHttp()
+            .getResponse()
+            .status(200)
+            .send({
+                ErrorCode: 0,
+                Result: null,
+                Sequence: Sequence + 1,
+            });
     }
 }
