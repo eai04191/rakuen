2285718461ed9ee95fa68a301720e57cebad2e4a
Irie Aoi <eai@mizle.net>
2022-03-07 20:56:12 +0900

    better prettier settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,7 @@ module.exports = {
     sourceType: "module",
   },
   plugins: ["@typescript-eslint/eslint-plugin"],
-  extends: [
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-  ],
+  extends: ["plugin:@typescript-eslint/recommended", "prettier"],
   root: true,
   env: {
     node: true,
